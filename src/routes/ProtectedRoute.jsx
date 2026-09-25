26a54074da4352fbfd3f46f25efe91bddcd65b0e
prashant kashyap <kashyapprashant62@gmail.com>
2026-09-25 07:19:57 +0530

    Move frontend into frontend folder

--- src/routes/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom';
-const ProtectedRoute = (props) => {
-    const token = localStorage.getItem("token");
-
-  return token ? props.children : <Navigate to={"/login"}/>
-}
-
-export default ProtectedRoute;
